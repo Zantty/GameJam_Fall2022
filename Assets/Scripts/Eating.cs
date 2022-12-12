54582bc075d343174a3eaa10605e479cd86a3b97
BlockSquad1000 <70970871+BlockSquad1000@users.noreply.github.com>
2022-12-12 13:46:59 -0500

    Added eating mechanic

--- Assets/Scripts/Eating.cs
+++ Assets/Scripts/Eating.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Eating : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Animal")
         {
-            Destroy(collision.gameObject);
+            if(Input.GetKey(KeyCode.E))
+            {
+                 Debug.Log("Ate an animal!");
+                 Destroy(collision.gameObject);
+            }           
         }
     }
 }
