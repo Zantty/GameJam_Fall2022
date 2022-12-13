6f56d7300239c6b73a0293862fdae562c0e2c88c
BlockSquad1000 <70970871+BlockSquad1000@users.noreply.github.com>
2022-12-13 12:12:47 -0500

    Player can only leave safe zone if they are flying

--- Assets/Scripts/DragonController.cs
+++ Assets/Scripts/DragonController.cs
@@ -14,6 +14,8 @@ public class DragonController : MonoBehaviour
 
     public bool flying;
     public bool dead;
+
+    public GameObject safeZoneBorder;
     void Start()
     {
         myAnim = GetComponent<Animator>();
@@ -35,6 +37,16 @@ public class DragonController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             flying = !flying;
+        }
+
+        if (flying)
+        {
+            safeZoneBorder.SetActive(false);
+        }
+
+        if(!flying)
+        {
+            safeZoneBorder.SetActive(true);
         }
     }
 }
